4e52f8b4f05b211767aaea71841017b811e6c4fe
julien garnon <garnon@hotmail.com>
2025-01-21 20:15:26 +0100

    remove the mapping to integrate with @embeded annotation

--- src/main/java/com/ju/library_ddd/catalog/domain/Book.java
+++ src/main/java/com/ju/library_ddd/catalog/domain/Book.java
@@ -1,13 +1,18 @@
 package com.ju.library_ddd.catalog.domain;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.util.Assert;
+import java.util.Objects;
 
-@Getter
+@Entity
 public class Book {
+    @EmbeddedId
     private BookId id;
     private String title;
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "isbn"))
     private Isbn isbn;
 
 
@@ -19,4 +24,22 @@ public class Book {
         this.isbn = isbn;
     }
 
+    public Book() {
+
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Book other = (Book) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/ju/library_ddd/catalog/infrastructure/BookEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ju.library_ddd.catalog.infrastructure;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface BookEntityRepository  extends CrudRepository<BookEntity, UUID> {
-}
