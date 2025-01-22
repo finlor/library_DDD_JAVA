e1ed9cc97dbbfeea452bc869bc16d43a3f3e84e4
julien garnon <garnon@hotmail.com>
2025-01-22 13:06:16 +0100

    add the useCase registerBook in application package

---
+++ src/main/java/com/ju/library_ddd/catalog/application/RegisterBookCopyUserCase.java
@@ -0,0 +1,18 @@
+package com.ju.library_ddd.catalog.application;
+
+import com.ju.library_ddd.catalog.domain.BarCode;
+import com.ju.library_ddd.catalog.domain.BookId;
+import com.ju.library_ddd.catalog.domain.Copy;
+import com.ju.library_ddd.catalog.domain.CopyRepository;
+
+public class RegisterBookCopyUserCase {
+
+    private final CopyRepository copyRepository;
+    public RegisterBookCopyUserCase(CopyRepository copyRepository) {
+        this.copyRepository = copyRepository;
+    }
+
+    public void register(BookId bookId, BarCode barCode) {
+        copyRepository.save(new Copy(barCode, bookId));
+    }
+}
