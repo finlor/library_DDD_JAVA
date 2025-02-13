c2c35cd3b0bd87294cf7c408aedf1e157e5d48a4
julien garnon <garnon@hotmail.com>
2025-02-13 09:19:55 +0100

    add comment

--- src/main/java/com/ju/library_ddd/lending/domain/Loan.java
+++ src/main/java/com/ju/library_ddd/lending/domain/Loan.java
@@ -29,12 +29,16 @@ public class Loan extends AbstractAggregateRoot<Loan> {
     public Loan(CopyId copyId, UserId userId, LoanRepository loanRepository) {
         Assert.notNull(copyId, "copyId must not be null");
         Assert.notNull(userId, "userId must not be null");
+        //super idee, on injecte le repository ici et ensuite on s assure que la copy est disponible dans le domain au lieu de le faire dans l'application layer
         Assert.isTrue(loanRepository.isAvailable(copyId), "loanRepository is available" );
         this.loanId = new LoanId();
         this.copyId = copyId;
         this.userId = userId;
         this.createdAt = LocalDateTime.now();
         this.expectedReturned = LocalDate.now().plusDays(15);
+
+        // This is because we extend from AbstractAggregateRoot, which has those methods to register events.
+        // And the event is registered, but it will only be fired when we hit save on the repository.
         this.registerEvent(new LoanCreated(this.copyId));
     }
 
